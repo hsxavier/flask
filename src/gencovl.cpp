8bcd0aed67f8dd7410ef9c469e89f4bd753a9760
Henrique S. Xavier <hsxavier@if.usp.br>
2014-11-10 14:23:40 +0000

    gencovl Completed
    In this version we added a configuration file scheme to gencovl. Both
    gencovl and corrlnfields use the same config file, which might be good
    or bad, let´s see. We also included an output of the order of the fields
    as variables in the covariance matrix. This version seems completed
    up to future changes when those are needed.
    
    Next step is to work on the corrlnfields routine.

--- src/gencovl.cpp
+++ src/gencovl.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstdlib>              // For function 'system'.
+#include "ParameterList.hpp"    // For configuration file.
 #include "Utilities.hpp"        // Error handling, tensor allocations.
 #include "interpol.h"           // Interpolation.
-
-#define  MAXINPUT 500
+#include <string.h>             // For strcpy.
+ 
+#define  MAXINPUT 100
 #define  NCLMAX   500
 
 
@@ -13,23 +15,31 @@
 int main (int argc, char *argv[]) {
   using std::cout; using std::endl; using std::string; using std::ofstream; // Basic stuff.
   using std::cin;
-  char mes1[MAXINPUT], mes2[MAXINPUT];
+  using namespace ParDef; ParameterList config;                             // Easy configuration file use.
+  char message[MAXINPUT], aux[MAXINPUT];
   std::string filename;
   std::ofstream outfile;                                                    // File for output.
-  std::ifstream infile, covfile;
-  int i, j, n, a1, a2, b1, b2, N1, N2, Nentries[NCLMAX], llout=5;
-  bool **IsSet;
+  std::ifstream infile;
+  int i, j, n, a1, a2, b1, b2, N1, N2, Nentries[NCLMAX], llout;
   long ncols, Nl, maxNl;
   double ***ll, ***Cov, *wrapper[2], **CovMatrix;
+  int **fnz; bool **IsSet, *fnzSet; 
   void getcovid(const std::string filename, int *a1, int *a2, int *b1, int *b2);
   void CountEntries(std::string filename, long *nr, long *nc);
 
-  // Listing files to use, given in command line.
-  mes1[0]='\0';
-  mes2[0]='\0';
-  for(i=1; i<argc; i++) sprintf(mes1,"%s %s", mes1, argv[i]);
-  sprintf(mes2, "ls%s > gencovl.temp", mes1);
-  system(mes2);
+  // Loading config file:
+  if (argc<=1) { cout << "You must supply a config file." << endl; return 0;}
+  config.load(argv[1]);
+  cout << endl;
+  cout << "-- Configuration setup:\n";
+  cout << "   File: "<<argv[1]<<endl;
+  config.lineload(argc, argv);
+  config.show();
+  cout << endl;
+
+  // Listing files to use based on CL_PREFIX:
+  sprintf(message, "ls %s* > gencovl.temp", config.reads("CL_PREFIX").c_str());
+  system(message);
 
   // Get file list and find out how many C(l)s there are:
   i=0; N1=0; N2=0, maxNl=0;
@@ -52,11 +62,15 @@ int main (int argc, char *argv[]) {
   
   // Allocate memory to store C(l)s:
   // First two indexes are CovMatrix indexes and last is for ll.
-  ll  = tensor3<double>(1, N1*N2, 1, N1*N2, 0, maxNl);
-  Cov = tensor3<double>(1, N1*N2, 1, N1*N2, 0, maxNl);
-  IsSet = matrix<bool>(1, N1*N2, 1, N1*N2); // For bookkepping.
+  // fnz stores the order that the fields are stored in CovMatrix.
+  fnz    =     matrix<int>(1, N1*N2, 1, 2);
+  fnzSet =    vector<bool>(1, N1*N2);           // For bookkeeping.
+  ll     = tensor3<double>(1, N1*N2, 1, N1*N2, 0, maxNl);
+  Cov    = tensor3<double>(1, N1*N2, 1, N1*N2, 0, maxNl);
+  IsSet  =    matrix<bool>(1, N1*N2, 1, N1*N2); // For bookkeeping.
   for(i=1; i<=N1*N2; i++) for(j=1; j<=N1*N2; j++) IsSet[i][j]=0;
-    
+  for(i=1; i<=N1*N2; i++) fnzSet[i]=0;
+  
   // Read C(l)s and store in data-cube:
   n=0;
   while (infile >> filename) {
@@ -64,6 +78,11 @@ int main (int argc, char *argv[]) {
     getcovid(filename, &a1, &a2, &b1, &b2);
     i=(a1-1)*N1+a2; j=(b1-1)*N1+b2;
     cout << filename << " goes to ["<<i<<", "<<j<<"]" << endl;
+    // Record the order of the fields in CovMatrix:
+    if (fnzSet[i]==0) { fnz[i][1] = a1; fnz[i][2] = a2; fnzSet[i] = 1; }
+    else if (fnz[i][1] != a1 || fnz[i][2] != a2) error("Field order in CovMatrix is messed up!"); 
+    if (fnzSet[j]==0) { fnz[j][1] = b1; fnz[j][2] = b2; fnzSet[j] = 1; }
+    else if (fnz[j][1] != b1 || fnz[j][2] != b2) error("Field order in CovMatrix is messed up!");
     // Import data:
     wrapper[0] = &(ll[i][j][0]);
     wrapper[1] = &(Cov[i][j][0]);
@@ -71,7 +90,17 @@ int main (int argc, char *argv[]) {
     IsSet[i][j]=1;
   };
   infile.close();
-  
+
+  // Check if every field was assigned a position in the CovMatrix:
+  for (i=1; i<=N1*N2; i++) if (fnzSet[i]==0) error("Some position in CovMatrix is unclaimed.");
+  free_vector(fnzSet, 1, N1*N2);
+  // If positions are OK, print them out:
+  outfile.open(config.reads("FIELD_LIST").c_str());
+  if (!outfile.is_open()) error("Cannot open FIELD_LIST file.");
+  PrintTable(fnz, N1*N2, 2, &outfile, 1);
+  outfile.close();
+  free_matrix(fnz, 1, N1*N2, 1, 2);
+
   // Set the rest of CovMatrix as a symmetric matrix:
   for(i=1; i<=N1*N2; i++) 
     for(j=1; j<=N1*N2; j++)
@@ -83,12 +112,15 @@ int main (int argc, char *argv[]) {
 	IsSet[i][j]=IsSet[j][i];
 	if (IsSet[i][j]==0) error ("Cannot set full CovMatrix, missing C(l).");
       }
+  free_matrix(IsSet,1, N1*N2, 1, N1*N2);
   
   // Interpolate and save covariance matrices for each ll:
+  llout = config.readi("LMAX");
+  strcpy(aux, config.reads("COVOUT_PREFIX").c_str());
   CovMatrix = matrix<double>(1, N1*N2, 1, N1*N2);
   for (n=1; n<=llout; n++) {
-    sprintf(mes1, "cov-ll%d.dat",n);
-    outfile.open(mes1);
+    sprintf(message, "%s%d.dat", aux, n);
+    outfile.open(message);
     for (i=1; i<=N1*N2; i++)
       for (j=1; j<=N1*N2; j++)
 	CovMatrix[i][j] = Interpol(&(ll[i][j][0]), maxNl, &(Cov[i][j][0]), (double)n);
@@ -97,6 +129,9 @@ int main (int argc, char *argv[]) {
   }
     
   // End of program:
+  free_tensor3(ll,1, N1*N2, 1, N1*N2, 0, maxNl);
+  free_tensor3(Cov,1, N1*N2, 1, N1*N2, 0, maxNl);
+  free_matrix(CovMatrix,1, N1*N2, 1, N1*N2);
   system("rm -f gencovl.temp");
   cout << "\nTotal number of warnings: " << warning("count") << endl;
   cout<<endl;
