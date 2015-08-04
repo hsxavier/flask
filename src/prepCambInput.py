ea41e4714aface78606d8668d333087f7db24f11
Henrique S. Xavier <hsxavier@if.usp.br>
2015-08-04 19:34:59 +0100

    prepCambInput.py actually created and working
    The python script to format CAMBsources output into input Cls for corrlnfields
    was created and tested by comparing with class output, and it works.

--- src/prepCambInput.py
+++ src/prepCambInput.py
@@ -2,80 +2,32 @@
 
 import numpy as np
 import sys
-import math as m
 
-# Get input
-classfile = sys.argv[1]
-outprefix = sys.argv[2]
+# Function to get position of Cl in CAMBsources Cov file:
+def CovPosition(f1, f2, Nfields):
+    return (Nfields+3)*(3+f1-1) + (3+f2)
 
-# Get file header:
-fp = open(classfile, "r")
-for i, line in enumerate(fp):
-    if i == 6:
-        header0 = line
-    elif i > 6:
-        break
-fp.close()
-header = header0.split()
-header.pop(0)
+# Get input:
+cambfile  = sys.argv[1]
+infofile  = sys.argv[2]
+outprefix = sys.argv[3]
+camb    = np.loadtxt(cambfile, unpack=True)
+info    = np.loadtxt(infofile)
+Nfields = info.shape[0]
+print 'Nfields =', Nfields
 
-# Find out if there is more than one field type:
-if "dens[" in header0 and "lens[" in header0:
-    NfieldTypes=2
-else:
-    NfieldTypes=1
+# Prepare factors:
+l   = camb[0]
+fac = (2.0*np.pi)/l/(l+1)
 
-# Generate output filenames:
-outfiles=[]
-for name in header:
-    ini=name.find(":")+1;
-    name = name[ini:]
-    if NfieldTypes==1:
-        name = name.replace("dens[","f1z")
-        name = name.replace("lens[","f1z")
-    else:
-        name = name.replace("dens[","f1z")
-        name = name.replace("lens[","f2z")
-    name = name.replace("]-","")
-    name = name.replace("]","")
-    name = outprefix+name+".dat"
-    outfiles.append(name)
-    
-# Load data:
-classout = np.loadtxt(classfile, unpack=True)
-ncols    = classout.shape[0]
-length   = classout.shape[1]
-
-# First column must be ell:
-l = classout[0]
-
-# Precompute basic factor:
-
-fac0 = (2.0*m.pi)/l/(l+1)
-fac1 = l*(l+1)/2.0   # The sign was supposed to be negative according to Hu 2000 (PRD 62:043007), 
-                     # but that would lead to negative kappa-delta cross-Cls, which according to 
-                     # numerical integration of delta auto-Cls is wrong. This is probably caused 
-                     # by differences in definitions.
-
-# LOOP over columns 
-for i in range(1, ncols):
-
-    # Discover if column is gal-gal, gal-lens ou lens-lens and compute appropriate factors:
-    factor = fac0
-    first = header[i].find("lens")
-    if first != -1:
-        # One lens so far, use fac1
-        factor = factor*fac1
-        second = header[i].find("lens", first+1)
-        if second !=-1:
-            # Two lens, use another fac1 
-            factor = factor*fac1
-    
-    # Multiply column by factor
-    classout[i] = classout[i]*factor
-    
-    # Export
-    print "Writing file "+outfiles[i]
-    np.savetxt(outfiles[i], np.transpose([l,classout[i]]))
+# LOOP over fields x fields:
+for f1 in range(1,Nfields+1):
+    for f2 in range(f1,Nfields+1):
+        # Prepare Cls:
+        Cl = np.transpose( [l, fac*camb[CovPosition(f1,f2,Nfields)]] )
+        # Export:
+        outfile = outprefix +'f'+str(int(info[f1-1][0]))+'z'+str(int(info[f1-1][1]))+'f'+str(int(info[f2-1][0]))+'z'+str(int(info[f2-1][1]))+'.dat'
+        print "Writing file "+outfile
+        np.savetxt(outfile, Cl)
 
 print "Done."
