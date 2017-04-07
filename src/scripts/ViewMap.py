f89231b565caef1873b8eddf517d14161171481c
Henrique S. Xavier <hsxavier@if.usp.br>
2017-04-07 13:47:27 -0300

    ViewMap takes multiple maps and manual updated
    Now the ViewMap.py script can take be used to open several maps at once.
    This was documented in the manual, which now also better describes the
    naming convention of non-separable selection function maps.

--- src/scripts/ViewMap.py
+++ src/scripts/ViewMap.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 
 """
-USAGE:   ViewMap.py <HEALPIX_MAP>
-EXAMPLE: ViewMap.py euclid_footprint.fits
+This script plots in Mollweide projection the Healpix maps saved as fits files 
+and passed to it as arguments. 
 
-This script plots in Mollweide projection a Healpix map saved as a fits file. 
+USAGE:   ViewMap.py <HEALPIX_MAP_1> <HEALPIX_MAP_2> ...
+EXAMPLE: ViewMap.py euclid_footprint.fits
 
 Written by: Henrique S. Xavier, hsxavier@if.usp.br, 23/apr/2015.
 """
@@ -14,14 +15,15 @@ import matplotlib.pyplot as plt
 import sys
 
 # Docstring output:
-if len(sys.argv) != 1 + 1: 
+if len(sys.argv) < 1 + 1: 
     print(__doc__)
     sys.exit(1)
 
 
 # Get input:
-infile = sys.argv[1]
-m      = hp.read_map(infile)
 
-hp.mollview(m)
+for infile in sys.argv[1:]:
+    m = hp.read_map(infile)
+    hp.mollview(m)
+
 plt.show()
