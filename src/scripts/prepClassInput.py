996b3368195bae59c0308f98953418fd4d8ad47b
Henrique S. Xavier <hsxavier@if.usp.br>
2018-05-16 19:18:42 -0300

    Generalized prepClassInput.py to UCLCl output
    UCLCl output has a similar format to CLASS', but with fewer header lines,
    it may output 'temp' columns and it starts Cl indexes at zero. I've made
    prepClassInput.py be able to handle any number of headers (all starting
    with hash and assuming the column names are the last one), to handle 'temp'
    (only tested with temp-only Cl table, but should work for combinations with
    'dens' and 'lens') and to change bin numbering starting from zero to one.
    
    I have noticed that there are operations made for columns 'phi', but there
    is no handling of this header.
    
    Also, the testing, documenting and commiting of the changes mentioned in
    the previous commit are stil to be done.

--- src/scripts/prepClassInput.py
+++ src/scripts/prepClassInput.py
@@ -17,6 +17,7 @@ Written by: Henrique S. Xavier, hsxavier@if.usp.br, 10/sep/2015.
 import numpy as np
 import sys
 import math as m
+import re
 
 # Docstring output:
 if len(sys.argv) != 1 + 2: 
@@ -28,39 +29,91 @@ if len(sys.argv) != 1 + 2:
 classfile = sys.argv[1]
 outprefix = sys.argv[2]
 
-# Get file header:
+
+
+#########################
+### Header operations ###
+#########################
+
+### Get file header (assumed to be last commented line):
 fp = open(classfile, "r")
-for i, line in enumerate(fp):
-    if i == 6:
-        header0 = line
-    elif i > 6:
+for line in fp:
+    if line[0]=='#':
+        header0=line
+    else:
         break
 fp.close()
-header = header0.split()
-header.pop(0)
+
+
+### Correct for missing space between # and column names:
+if header0[1]!=' ':
+    header0 = header0[0]+' '+header0[1:]
+
+### Add one to bin numbering if they start at zero (find such situation by looking for a [0] bin):
+def addOne(s):
+    if s.isdigit():
+        return '['+str(int(s)+1)+']'
+    else:
+        return s
+# Look for a zeroth bin, and do the dance:
+if '[0]' in header0:
+    header0 = ''.join([addOne(s) for s in re.split('\[|\]',header0)])
+
 
 # Find out if there is more than one field type:
-if "dens[" in header0 and "lens[" in header0:
-    NfieldTypes=2
+if "dens[" in header0 and "lens[" in header0 and "temp[" in header0:
+    FieldTypes=3
+elif "dens[" in header0 and "lens[" in header0:
+    FieldTypes=2
+elif "dens[" in header0 and "temp[" in header0:
+    FieldTypes=4
+elif "lens[" in header0 and "temp[" in header0:
+    FieldTypes=6
 else:
-    NfieldTypes=1
+    FieldTypes=1
 
+# Select column names from header:
+header = header0.split()
+header.pop(0)
 # Generate output filenames:
+# Let's adopt the field type priority in numbering: temperature, density and lensing.
 outfiles=[]
 for name in header:
     ini=name.find(":")+1;
     name = name[ini:]
-    if NfieldTypes==1:
+    if FieldTypes==1:
+        # Only one field type:
         name = name.replace("dens[","f1z")
         name = name.replace("lens[","f1z")
-    else:
+        name = name.replace("temp[","f1z")
+    elif FieldTypes==2:
+        # Density and convergence:
         name = name.replace("dens[","f1z")
         name = name.replace("lens[","f2z")
+    elif FieldTypes==4:
+        # Temperature and density:
+        name = name.replace("temp[","f1z")
+        name = name.replace("dens[","f2z")
+    elif FieldTypes==6:
+        # Temperature and convergence:
+        name = name.replace("temp[","f1z")
+        name = name.replace("lens[","f2z") 
+    elif FieldTypes==3:
+        # Temperature, density and convergence:
+        name = name.replace("temp[","f1z")
+        name = name.replace("dens[","f2z")
+        name = name.replace("lens[","f3z")
     name = name.replace("]-","")
     name = name.replace("]","")
     name = outprefix+name+".dat"
     outfiles.append(name)
-    
+
+
+
+#######################
+### Data operations ###
+#######################    
+
 # Load data:
 classout = np.loadtxt(classfile, unpack=True)
 ncols    = classout.shape[0]
@@ -82,6 +135,7 @@ for i in range(1, ncols):
     factor = fac0
 
     # Discover if column is gal-gal, gal-lens ou lens-lens and compute appropriate factors:
+    # (Factor-wise, temperature is treated as galaxy)
     first = header[i].find("lens") 
     if first != -1:
         # One lens so far, use fac1
